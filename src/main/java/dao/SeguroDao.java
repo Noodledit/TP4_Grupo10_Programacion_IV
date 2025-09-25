794c4d58b2d07a341b9ee5fd6c87798431a10f9e
Miguel Velásquez <86170247+MrM1nd91@users.noreply.github.com>
2025-09-25 11:27:36 -0300

    Filtro

--- src/main/java/dao/SeguroDao.java
+++ src/main/java/dao/SeguroDao.java
@@ -15,6 +15,7 @@ import java.sql.ResultSet;
 public class SeguroDao {
 	
 	public List<TipoSeguro> listarTipoSeguros(){
+		
 		List<TipoSeguro> ListaTipoSeguro = new ArrayList<>();
 		
 		String consulta = "SELECT * FROM tiposeguros";
@@ -27,6 +28,7 @@ public class SeguroDao {
 				TipoSeguro ts = new TipoSeguro();
 				ts.setIdTipo(Listado.getInt("idTipo"));
 				ts.setDescripcion(Listado.getString("descripcion"));			
+				ListaTipoSeguro.add(ts);
 			}
 			
 			
@@ -39,5 +41,36 @@ public class SeguroDao {
 
 		
 	}
+	
+	public List<TipoSeguro> FiltrarTipoSeguros(int id ){
+		List<TipoSeguro> ListaTipoSeguro = new ArrayList<>();
+		
+		String consulta = "SELECT * FROM tipoSeguros WHERE idTipo = ?";
+		
+		try(Connection conn = Conexion.getConnection();
+		PreparedStatement pst = conn.prepareStatement(consulta)){
+			
+			pst.setInt(1, id);
+			
+			ResultSet Listado = pst.executeQuery();
+			
+			while(Listado.next()) {
+				TipoSeguro ts = new TipoSeguro();
+				ts.setIdTipo(Listado.getInt("idTipo"));
+				ts.setDescripcion(Listado.getString("descripcion"));		
+				ListaTipoSeguro.add(ts);
+			}
+			
+			
+		}catch (Exception e) {
+			e.printStackTrace();
+			
+		}
+		
+		return ListaTipoSeguro;
+
+		
+	}
+
 
 }
